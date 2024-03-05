50794fb57022403fe0f622f32955edd0ba9a0f74
MariiaSam <kontragent@ukr.net>
2024-03-05 18:22:24 +0200

    1 1.1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,32 @@
-import './App.css';
+import "./App.css";
+
+const pageTitle = "Page title";
+const welcomeText = "Welcome to React";
+const nullVar = null;
+const undefindVar = undefined;
+
+const username = "admin";
+
+const arr = [1, 2, 3]
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <div className="main">
+        <p>{pageTitle}</p>
+        <label htmlFor="id">password</label>
+        <input className="field" type="passwoprd" autoFocus />
+        <p>{welcomeText}</p>
+        <p>
+          {username === "admin"
+            ? "Welcome to site config"
+            : "Need permission to access"}
+        </p>
+        <p>{nullVar}</p>
+        <p>{undefindVar}</p>
+        <div>{arr}</div>
+      </div>
+    </>
   );
 }
 
