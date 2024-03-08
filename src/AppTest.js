050ed52b0f7b2787b02e3f4b9d2cc3dc8cd2a05f
MariiaSam <kontragent@ukr.net>
2024-03-08 13:11:17 +0200

    corr

--- src/AppTest.js
+++ src/AppTest.js
@@ -1,7 +1,6 @@
 
 
 
-
 // import "./App.css";
 
 // const pageTitle = "Page title";
--- src/components/MovieList/MovieListItem.jsx
+++ src/components/MovieList/MovieListItem.jsx
@@ -1,13 +1,13 @@
 
 const MovieListItem = ({ title, year}) => {
-return (<li>{title}
-    <div>
-      <img src="" alt="" />
-    </div>
-    <h4> {title}</h4>
-    <p> Year, {year}</p>
-    </li> )
-
-}
-
-export default MovieListItem
+    return (<li>{title}
+        <div>
+          <img src="" alt="" />
+        </div>
+        <h4> {title}</h4>
+        <p> Year, {year}</p>
+        </li> )
+    
+    }
+    
+    export default MovieListItem
